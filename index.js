823d6aac4696b6178b6a17a19e678cff9493d632
dhoo <122699620+dhooHub@users.noreply.github.com>
2026-01-13 18:45:32 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("OK - TICO-bot vivo ✅"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Running"));
