312798241ae5dbcb59f799eda826b361118f2981
dhoo <122699620+dhooHub@users.noreply.github.com>
2026-01-13 18:49:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ app.get("/", (req, res) => res.send("OK - TICO-bot vivo ✅"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Running"));
+app.get("/webhook", (req, res) => {
+  res.send("Webhook activo");
+});
+
