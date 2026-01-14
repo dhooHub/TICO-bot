203663d09111074391f381ed038b0efc78f62ec3
dhoo <122699620+dhooHub@users.noreply.github.com>
2026-01-14 16:56:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get("/", (req, res) => res.send("OK - TICO-bot vivo ✅"));
 
 app.get("/webhook", (req, res) => {
   const VERIFY_TOKEN = "tico_verify_123";
+  app.post("/webhook", (req, res) => {
+  console.log("Mensaje recibido:", JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
 
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
@@ -20,6 +25,7 @@ app.get("/webhook", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Running"));
+
 
 
 
